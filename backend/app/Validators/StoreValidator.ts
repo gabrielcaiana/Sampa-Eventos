9ffa54dbebe3df0f56025d4f1e0d0646b27b23ce
“gabrielcaiana” <gabrielcaiana@hotmail.com>
2021-06-07 09:47:43 -0300

    validator de login

---
+++ backend/app/Validators/StoreValidator.ts
@@ -0,0 +1,14 @@
+import { schema, rules } from '@ioc:Adonis/Core/Validator'
+import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+
+export default class StoreValidator {
+  constructor (protected ctx: HttpContextContract) {
+  }
+
+  public schema = schema.create({
+		email: schema.string({trim: true}),
+		password: schema.string({trim: true}, [rules.minLength(6), rules.maxLength(10)])
+  })
+
+  public messages = {}
+}
